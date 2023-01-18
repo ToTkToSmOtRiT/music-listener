e41c59168eb7cec7918bb70b0209105e4acd9c23
Marshall <zz90847@gmail.com>
2023-01-18 18:58:05 +0300

    renamed:    src/assets/music/Killswitch Engage - Rose Of Sharyn (Piano Cover).mp3 -> public/assets/music/Killswitch Engage - Rose Of Sharyn (Piano Cover).mp3 	renamed:    src/assets/music/Killswitch engage - Arms of Sorrow(piano ver.).mp3 -> public/assets/music/Killswitch engage - Arms of Sorrow(piano ver.).mp3 	renamed:    src/assets/music/Killswitch engage - My curse(piano ver.).mp3 -> public/assets/music/Killswitch engage - My curse(piano ver.).mp3 	renamed:    src/assets/music/Killswitch-engage - Arms of Sorrow piano ver.ogg -> public/assets/music/Killswitch-engage - Arms of Sorrow piano ver.ogg 	new file:   scss/styles.scss 	new file:   src/assets/icon1.png 	new file:   src/assets/icon2.png 	new file:   src/assets/icon3.png 	modified:   src/components/MusicSection.vue

--- src/components/MusicSection.vue
+++ src/components/MusicSection.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <audio src="./assets/music/Killswitch engage - Arms of Sorrow(piano ver.).mp3" ></audio>
+    <div id="player">
+        <p>Killswitch engage - Arms of Sorrow(piano ver.).mp3</p>
+        <!-- <audio :class="song" src="./assets/music/Killswitch engage - Arms of Sorrow(piano ver.).mp3" ></audio> -->
+        <div class="buttons">
+            <img class="prev" src="@/assets/icon2.png" alt="Previous">
+            <img class="play" @click="playSong('assets/music/Killswitch engage - Arms of Sorrow(piano ver.).mp3')"
+                src="@/assets/icon1.png" alt="Play">
+            <img class="next" src="@/assets/icon3.png" alt="Next ">
+        </div>
     </div>
 </template>
 
@@ -9,8 +16,20 @@
 
 export default {
     name: 'MusicSection',
+    el: '#player',
     components: {
 
+    },
+    methods: {
+        playSong(song) {
+            if (song) {
+                var audio = new Audio(song)
+                audio.play()
+            }
+        },
+        stopSong() {
+
+        }
     }
 }
 
@@ -18,5 +37,7 @@ export default {
 
 
 <style>
+
+
 
 </style>
