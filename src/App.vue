748bc917af644382658a094bfca549914191d60f
Marshall <zz90847@gmail.com>
2023-01-23 12:08:37 +0300

    modified:   package-lock.json 	modified:   package.json 	deleted:    scss/styles.scss 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #1e699b;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #1e699b;
+    margin-top: 60px;
 }
 </style>
