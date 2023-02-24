9c6ce70e346aefa741d052a8f3ef86ffe02f80ff
Marshall <zz90847@gmail.com>
2023-02-24 12:03:58 +0300

    wip, feature: added authentication plugin

---
+++ src/api/auth.js
@@ -0,0 +1,13 @@
+export default function(instance){
+    return{
+        signIn(payLoad){
+            return instance.post('auth/sign_in', payLoad)
+        },
+        signUp(payload){
+            return instance.post('auth/sign_up', payload)
+        },
+        logout(){
+            return instance.delete('auth/logout')
+        }
+    }
+}
--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,7 @@
-import axios from "axios";
+import instance from "./instance";
 
-const options = {
-  method: 'GET',
-  url: 'https://spotify23.p.rapidapi.com/tracks/',
-  params: {ids: '4WNcduiCmDNfmTEz7JvmLv'},
-  headers: {
-    'X-RapidAPI-Key': '90894375c6mshd8c4de8ac82ee4fp1ac5bdjsnb5e826acf7e2',
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-  }
-};
+import authModule from "./auth";
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+export default{
+    auth: authModule(instance)
+}
